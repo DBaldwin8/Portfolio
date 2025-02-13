de0423977806833f965079d0c1fcc227b1087a79
Darren <darren_baldwin@live.com>
2025-02-13 09:49:54 +0000

    index, removed text from second h2 and added id, script, working on timeout fired by onload and else resets i in typing function

--- script.js
+++ script.js
@@ -4,38 +4,38 @@
 //     location.reload();
 //   }, 1500);
 
-let speed = 130;
 let i = 0;
 
-// function typing(string, selectorId) {
-//     if (i < string.length) {
-//         document.getElementById(selectorId).innerHTML += string.charAt(i);
-//         i++;
-//         setTimeout(function () { typing(string, selectorId) }, speed);
-//     } 
-// }
-
-
-
-// window.onload = async function () {
-//     setTimeout(function () { typing("Hello World,", "hello") }, 2000)
-//     await function() {typing("My name is ", "name")};
-// }
-
-function typing(string) {
-    if (i < 12) {
-        document.getElementById("hello").innerHTML += string.charAt(i);
+function typing(string, selectorId) {
+    if (i < string.length) {
+        document.getElementById(selectorId).innerHTML += string.charAt(i);
         i++;
-        setTimeout(function () { typing(string) }, speed);
-    } else if (i < string.length) {
-        document.getElementById("name").innerHTML += string.charAt(i);
-        i++;
-        setTimeout(function () { typing(string) }, speed);
+        setTimeout(function () { typing(string, selectorId) }, 150);
+    } else {
+        i = 0;
     }
 }
 
-
-
 window.onload = async function () {
-    setTimeout(function () { typing("Hello World,My name is Darren Baldwin") }, 2000);
+    setTimeout(function () { typing("Hello World,", "hello") }, 2000);
+    setTimeout(function () { typing("My name is Darren Baldwin", "name") }, 4500);
+    setTimeout(function () { typing("and I am a FULL STACK DEVELOPER.", "fst") }, 8500);
 }
+
+// function typing(string) {
+//     if (i < 12) {
+//         document.getElementById("hello").innerHTML += string.charAt(i);
+//         i++;
+//         setTimeout(function () { typing(string) }, speed);
+//     } else if (i < string.length) {
+//         document.getElementById("name").innerHTML += string.charAt(i);
+//         i++;
+//         setTimeout(function () { typing(string) }, speed);
+//     }
+// }
+
+// window.onload = function () {
+//     setTimeout(function () { 
+//         typing("Hello World, My name is Darren Baldwin and I am a FULL STACK DEVELOPER") }
+//         , 1000);
+// }
