377aff5d9f4de7a9f5b1f19c541f52a851298f81
Darren <darren_baldwin@live.com>
2025-02-13 09:54:03 +0000

    changed timing

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function typing(string, selectorId) {
 
 window.onload = async function () {
     setTimeout(function () { typing("Hello World,", "hello") }, 1000);
-    setTimeout(function () { typing("My name is Darren Baldwin", "name") }, 4500);
-    setTimeout(function () { typing("and I am a FULL STACK DEVELOPER.", "fst") }, 8500);
+    setTimeout(function () { typing("My name is Darren Baldwin", "name") }, 3500);
+    setTimeout(function () { typing("and I am a FULL STACK DEVELOPER.", "fst") }, 7500);
 }
 
 // function typing(string) {
