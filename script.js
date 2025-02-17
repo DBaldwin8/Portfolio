eb4dd5814d4a30b342e98dd9255594cb54b3243d
Darren <darren_baldwin@live.com>
2025-02-17 16:42:34 +0000

    using for loop, i as param, timeout has math to correct delay

--- script.js
+++ script.js
@@ -2,22 +2,33 @@
 
 /******** Typing function ********/
 
+// async function typing(string, selectorId) {
+//     return new Promise((resolve) => {
+//         let i = 0;
+//         function typeCharacter() {
+//             if (i < string.length) {
+//                 document.getElementById(selectorId).textContent += string.charAt(i);
+//                 i++;
+//                 setTimeout(typeCharacter, 150); // This is the delay between characters
+//             } else {
+//                 setTimeout(resolve, 1000); // This is the delay to the next call.
+//             }
+//         }
+//         typeCharacter();
+//     });
+// };
 
 async function typing(string, selectorId) {
     return new Promise((resolve) => {
-        let i = 0;
-        function typeCharacter() {
-            if (i < string.length) {
-                document.getElementById(selectorId).textContent += string.charAt(i);
-                i++;
-                setTimeout(typeCharacter, 150);
-            } else {
-                setTimeout(resolve, 1000);
-            }
+        function typeCharacter(i) {
+            document.getElementById(selectorId).textContent += string.charAt(i);
         }
-        typeCharacter();
-    });
-};
+        for (let i = 0; i < string.length; i++) {
+            setTimeout(function () {typeCharacter(i)}, i*150);
+        }
+        setTimeout(resolve, string.length * 150 + 1000); // This is the delay to the next call.
+    })
+}
 
 /******** Onload instructions ********/
 
@@ -25,7 +36,7 @@ const deviceHeight = window.innerHeight;
 
 window.onload = async function () {
     await typing("Hello World,", "hello");
-    await typing("My name is ", "name");
-    await typing("and I am a .", "fst");
+    await typing("My name is Darren Baldwin", "name");
+    await typing("and I am a FULL STACK DEVELOPER.", "fst");
     await window.scroll(0, deviceHeight);
 }
