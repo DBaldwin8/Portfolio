b5a99ef4c91b79f9794561136ddf3c67bb97ed91
Darren <darren_baldwin@live.com>
2025-02-17 13:41:05 +0000

    clean up old comments

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 'use strict'
 
-// setTimeout(() => {
-//     location.reload();
-//   }, 1500);
-
 /******** Typing function ********/
 
 
@@ -29,21 +25,3 @@ window.onload = async function () {
     setTimeout(function () { typing("and I am a FULL STACK DEVELOPER.", "fst") }, 7500);
     setTimeout(function () { window.scroll(0, deviceHeight) }, 13000);
 }
-
-// function typing(string) {
-//     if (i < 12) {
-//         document.getElementById("hello").innerHTML += string.charAt(i);
-//         i++;
-//         setTimeout(function () { typing(string) }, speed);
-//     } else if (i < string.length) {
-//         document.getElementById("name").innerHTML += string.charAt(i);
-//         i++;
-//         setTimeout(function () { typing(string) }, speed);
-//     }
-// }
-
-// window.onload = function () {
-//     setTimeout(function () { 
-//         typing("Hello World, My name is Darren Baldwin and I am a FULL STACK DEVELOPER") }
-//         , 1000);
-// }
