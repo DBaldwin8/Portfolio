c7f4c49e3888ea463c72c1d9fa2e9d027a229821
Darren <darren_baldwin@live.com>
2025-02-17 13:22:53 +0000

    typeCharacter function in typing, scroll timed right

--- script.js
+++ script.js
@@ -6,27 +6,28 @@
 
 /******** Typing function ********/
 
-let i = 0;
 
 function typing(string, selectorId) {
-    if (i < string.length) {
-        document.getElementById(selectorId).textContent += string.charAt(i);
-        i++;
-        setTimeout(function () { typing(string, selectorId) }, 150);
-    } else {
-        i = 0;
+    let i = 0;
+    function typeCharacter() {
+        if (i < string.length) {
+            document.getElementById(selectorId).textContent += string.charAt(i);
+            i++;
+            setTimeout(typeCharacter, 150);
+        }
     }
+    typeCharacter();
 }
 
 /******** Onload instructions ********/
 
 const deviceHeight = window.innerHeight;
 
-window.onload = function () {
+window.onload = async function () {
     setTimeout(function () { typing("Hello World,", "hello") }, 1000);
     setTimeout(function () { typing("My name is Darren Baldwin", "name") }, 3500);
     setTimeout(function () { typing("and I am a FULL STACK DEVELOPER.", "fst") }, 7500);
-    setTimeout(function () { window.scroll(0, deviceHeight)},1000);
+    setTimeout(function () { window.scroll(0, deviceHeight) }, 13000);
 }
 
 // function typing(string) {
